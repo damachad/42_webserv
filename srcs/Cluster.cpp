8b78c24a2e1b198fcb3f271e6d87139147103ab5
tiaferna <tiaferna@student.42.fr>
2024-09-24 15:39:22 +0100

    added check for .py extension

--- srcs/Cluster.cpp
+++ srcs/Cluster.cpp
@@ -18,6 +18,7 @@
 #include "HTTPRequestParser.hpp"
 #include "PostResponse.hpp"
 #include "RequestErrorResponse.hpp"
+#include "CGI.hpp"
 
 // Constructor
 // Create a vector of servers from provided context vector
@@ -204,6 +205,11 @@ const std::string Cluster::get_response(const HTTP_Request& request,
 	if (error_status != OK)
 		response_check =
 			new RequestErrorResponse(server, request, error_status);
+	else if (request.uri.length() > 3 && request.uri.substr(request.uri.length() - 3) == ".py")
+	{
+		CGI cgi(request);
+		return cgi.handleCGIResponse();
+	}
 	else {
 		switch (static_cast<int>(request.method)) {
 			case (GET):
