3e42c73dba3c47844fa3fe313a7674c6bdab30d8
damachad <damachad@student.42porto.com>
2024-10-03 12:36:15 +0100

    Implement download file in specific directory

--- srcs/GetResponse.cpp
+++ srcs/GetResponse.cpp
@@ -6,7 +6,7 @@
 /*   By: damachad <damachad@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/13 15:14:52 by damachad          #+#    #+#             */
-/*   Updated: 2024/10/02 14:41:50 by damachad         ###   ########.fr       */
+/*   Updated: 2024/10/03 12:35:00 by damachad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,20 +25,33 @@ GetResponse::GetResponse(const Server& server, const HTTP_Request& request)
 short GetResponse::loadFile(const std::string& path) {
 	std::ifstream file(path.c_str());
 	if (!file.is_open()) return 500;
+	// std::multimap<std::string, std::string>headers = _request.header_fields;
+	// std::multimap<std::string, std::string>::const_iterator it_modified = headers.find("if-modified-since"); 
+	// NOTE: headers with spaces not parsed for this
+	// if (it_modified != headers.end()) {
+	// 	std::string last_modified = getLastModificationDate(path);
+	// 	if (parseTime(it_modified->second) >= parseTime(last_modified))
+	// 		return 304;
+	// }
 	_response.body.assign((std::istreambuf_iterator<char>(file)),
 						  (std::istreambuf_iterator<char>()));
 	file.close();
 	loadCommonHeaders();
+	// If download in URI, GET request will download file
+	if (_request.uri.find("download") != std::string::npos) {
+		size_t posSlash = _request.uri.find_last_of("/");
+		std::string fileName = _request.uri.substr(posSlash + 1);
+		if (fileName.empty()) fileName = "download";
+		_response.headers.insert(
+			std::make_pair(std::string("Content-Disposition"), std::string("attachment; filename=\"" + fileName + "\"")));
+	}
 	setMimeType(path);
 	return 200;
 }
 
 std::string GetResponse::generateResponse() {
 	setMatchLocationRoute();
-	short status;
-	// status = checkSize();
-	// if (status != 200) return loadErrorPage(status);
-	status = checkMethod();
+	short status = checkMethod();
 	if (status != 200) return loadErrorPage(status);
 	if (hasReturn()) {
 		loadReturn();
