830c12c7e996f198de9172abe24b710e8e6f369f
damachad <damachad@student.42porto.com>
2024-10-16 14:00:25 +0100

    Update configs parser error messages

--- srcs/Server.cpp
+++ srcs/Server.cpp
@@ -104,7 +104,7 @@ static bool isValidIp(const std::string &ip) {
 // Handlers
 void Server::handleListen(std::vector<std::string> &tokens) {
   if (tokens.size() > 2)
-    throw ConfigError("Too many arguments in listen directive.");
+    throw ConfigError("Invalid number of arguments in listen directive.");
   std::vector<std::string>::const_iterator
       it; // check if accept just one token (if not implementing
           // default_server)
@@ -116,7 +116,7 @@ void Server::handleListen(std::vector<std::string> &tokens) {
       listening.IP = value.substr(0, colonPos);
       listening.port = value.substr(colonPos + 1);
       if (listening.IP.empty() || listening.port.empty())
-        throw ConfigError("Invalid IP:Port.");
+        throw ConfigError("Invalid IP:Port in listen directive.");
     } else {
       if ((*it).find_first_not_of("0123456789") == std::string::npos)
         listening.port = value;
@@ -128,7 +128,7 @@ void Server::handleListen(std::vector<std::string> &tokens) {
     if (listening.port.empty())
       listening.port = "80"; // default
     if (!isValidIp(listening.IP) || !isValidPort(listening.port))
-      throw ConfigError("Invalid IP:Port.");
+      throw ConfigError("Invalid IP:Port in listen directive.");
     _network_address.push_back(listening);
   }
 }
@@ -140,7 +140,7 @@ void Server::handleServerName(std::vector<std::string> &tokens) {
 
 void Server::handleRoot(std::vector<std::string> &tokens) {
   if (tokens.size() > 2)
-    throw ConfigError("Too many arguments in root directive.");
+    throw ConfigError("Invalid number of arguments in root directive.");
   _root = tokens[1];
 }
 
@@ -151,7 +151,7 @@ void Server::handleIndex(std::vector<std::string> &tokens) {
 
 void Server::handleErrorPage(std::vector<std::string> &tokens) {
   if (tokens.size() < 3)
-    throw ConfigError("Invalid error_page directive.");
+    throw ConfigError("Invalid number of arguments in error_page directive.");
   std::string page = tokens.back();
   for (size_t i = 1; i < tokens.size() - 1; i++) {
     char *end;
@@ -165,7 +165,7 @@ void Server::handleErrorPage(std::vector<std::string> &tokens) {
 
 void Server::handleCliMaxSize(std::vector<std::string> &tokens) {
   if (tokens.size() != 2)
-    throw ConfigError("Invalid client_max_body_size directive.");
+    throw ConfigError("Invalid number of arguments in client_max_body_size directive.");
   std::string maxSize = tokens[1];
   char unit = maxSize[maxSize.size() - 1];
   if (!std::isdigit(unit))
@@ -175,7 +175,7 @@ void Server::handleCliMaxSize(std::vector<std::string> &tokens) {
   char *endPtr = NULL;
   long size = std::strtol(maxSize.c_str(), &endPtr, 10);
   if (*endPtr != '\0')
-    throw ConfigError("Invalid numeric value for client_max_body_size.");
+    throw ConfigError("Invalid value for client_max_body_size.");
   _clientMaxBodySize = size;
   if (!std::isdigit(unit)) {
     // check for overflow during multiplication
@@ -211,11 +211,11 @@ void Server::handleAutoIndex(std::vector<std::string> &tokens) {
   else if (tokens[1] == "off")
     _autoIndex = FALSE;
   else
-    throw ConfigError("Invalid syntax.");
+    throw ConfigError("Invalid value in autoindex directive.");
 }
 
 void Server::handleReturn(std::vector<std::string> &tokens) {
-	if (tokens.size() != 3) throw ConfigError("Invalid return directive.");
+	if (tokens.size() != 3) throw ConfigError("Invalid number of arguments in return directive.");
 	// check if there is overflow
 	char *endPtr = NULL;
 	long errorCode = std::strtol(tokens[1].c_str(), &endPtr, 10);
@@ -229,13 +229,13 @@ void Server::handleReturn(std::vector<std::string> &tokens) {
 
 void Server::handleUpload(std::vector<std::string> &tokens) {
 	if (tokens.size() != 2)
-		throw ConfigError("Invalid upload_store directive.");
+		throw ConfigError("Invalid number of arguments in upload_store directive.");
 	_uploadStore = tokens[1];
 }
 
 void Server::handleCgiExt(std::vector<std::string> &tokens) {
   if (tokens.size() > 2)
-    throw ConfigError("Too many arguments in cgi_ext directive.");
+    throw ConfigError("Invalid number of arguments in cgi_ext directive.");
   _cgiExt = tokens[1];
 }
 
@@ -243,13 +243,13 @@ void Server::processDirective(std::string &line) {
   std::vector<std::string> tokens;
   tokens = ConfigParser::tokenizeLine(line);
   if (tokens.size() < 2)
-    throw ConfigError("No value for directive: " + tokens[0]);
+    throw ConfigError("Invalid number of arguments in \"" + tokens[0] + "\" directive.");
   std::map<std::string, DirectiveHandler>::const_iterator it;
   it = _directiveMap.find(tokens[0]);
   if (it != _directiveMap.end())
     (this->*(it->second))(tokens);
   else
-    throw ConfigError("Unkown directive: " + tokens[0]);
+    throw ConfigError("Unkown directive \"" + tokens[0] + "\"");
 }
 
 // Loads a location block and adds it to the map in server
